47b636d616bd7b2335dd389b761b22822b3ed3d7
zwang2244 <zwang2244@wisc.edu>
2021-11-08 20:32:06 -0600

    Trending page and top games page done

--- app/src/main/java/edu/illinois/cs465/boardrate/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/edu/illinois/cs465/boardrate/ui/dashboard/DashboardFragment.java
@@ -28,13 +28,13 @@ public class DashboardFragment extends Fragment {
         binding = FragmentDashboardBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textDashboard;
-        dashboardViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+//        final TextView textView = binding.textDashboard;
+//        dashboardViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+//            @Override
+//            public void onChanged(@Nullable String s) {
+//                textView.setText(s);
+//            }
+//        });
         return root;
     }
 
