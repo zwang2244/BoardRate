23e1fc198360f6ad31b860a6e41a58fd4a28a1a0
zwang2244 <zwang2244@wisc.edu>
2021-11-12 13:58:32 -0600

    Import game page data

---
+++ app/src/main/java/edu/illinois/cs465/boardrate/Game.java
@@ -0,0 +1,173 @@
+package edu.illinois.cs465.boardrate;
+
+/**
+ * Game type/class stores info
+ * */
+public class Game {
+    private int gameID;
+    private String Name;
+    private String Rating;
+    private int NumberofReviewrs;
+    private String ImageURL;
+    private String TimetoPlay;
+    private int Ranking1;
+    private int Rankings2;
+    private String NumberofPlayers;
+//    private String Description;
+    private String Tag1;
+    private String Tag2;
+    private String Tag3;
+    private int Tag1Ranking;
+    private int Tag2Ranking;
+    private int Tag3Ranking;
+
+    public int getGameID() {
+        return gameID;
+    }
+
+    public void setGameID(int gameID) {
+        this.gameID = gameID;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public String getRating() {
+        return Rating;
+    }
+
+    public void setRating(String rating) {
+        Rating = rating;
+    }
+
+    public int getNumberofReviewrs() {
+        return NumberofReviewrs;
+    }
+
+    public void setNumberofReviewrs(int numberofReviewrs) {
+        NumberofReviewrs = numberofReviewrs;
+    }
+
+    public String getImageURL() {
+        return ImageURL;
+    }
+
+    public void setImageURL(String imageURL) {
+        ImageURL = imageURL;
+    }
+
+    public String getTimetoPlay() {
+        return TimetoPlay;
+    }
+
+    public void setTimetoPlay(String timetoPlay) {
+        TimetoPlay = timetoPlay;
+    }
+
+    public int getRanking1() {
+        return Ranking1;
+    }
+
+    public void setRanking1(int ranking1) {
+        Ranking1 = ranking1;
+    }
+
+    public int getRankings2() {
+        return Rankings2;
+    }
+
+    public void setRankings2(int rankings2) {
+        Rankings2 = rankings2;
+    }
+
+    public String getNumberofPlayers() {
+        return NumberofPlayers;
+    }
+
+    public void setNumberofPlayers(String numberofPlayers) {
+        NumberofPlayers = numberofPlayers;
+    }
+
+//    public String getDescription() {
+//        return Description;
+//    }
+//
+//    public void setDescription(String description) {
+//        Description = description;
+//    }
+
+    public String getTag1() {
+        return Tag1;
+    }
+
+    public void setTag1(String tag1) {
+        Tag1 = tag1;
+    }
+
+    public String getTag2() {
+        return Tag2;
+    }
+
+    public void setTag2(String tag2) {
+        Tag2 = tag2;
+    }
+
+    public String getTag3() {
+        return Tag3;
+    }
+
+    public void setTag3(String tag3) {
+        Tag3 = tag3;
+    }
+
+    public int getTag1Ranking() {
+        return Tag1Ranking;
+    }
+
+    public void setTag1Ranking(int tag1Ranking) {
+        Tag1Ranking = tag1Ranking;
+    }
+
+    public int getTag2Ranking() {
+        return Tag2Ranking;
+    }
+
+    public void setTag2Ranking(int tag2Ranking) {
+        Tag2Ranking = tag2Ranking;
+    }
+
+    public int getTag3Ranking() {
+        return Tag3Ranking;
+    }
+
+    public void setTag3Ranking(int tag3Ranking) {
+        Tag3Ranking = tag3Ranking;
+    }
+
+    @Override
+    public String toString() {
+        return "Game{" +
+                "gameID=" + gameID +
+                ", Name='" + Name + '\'' +
+                ", Rating='" + Rating + '\'' +
+                ", NumberofReviewrs=" + NumberofReviewrs +
+                ", ImageURL='" + ImageURL + '\'' +
+                ", TimetoPlay='" + TimetoPlay + '\'' +
+                ", Ranking1=" + Ranking1 +
+                ", Rankings2=" + Rankings2 +
+                ", NumberofPlayers='" + NumberofPlayers + '\'' +
+//                ", Description='" + Description + '\'' +
+                ", Tag1='" + Tag1 + '\'' +
+                ", Tag2='" + Tag2 + '\'' +
+                ", Tag3='" + Tag3 + '\'' +
+                ", Tag1Ranking=" + Tag1Ranking +
+                ", Tag2Ranking=" + Tag2Ranking +
+                ", Tag3Ranking=" + Tag3Ranking +
+                '}';
+    }
+}
--- app/src/main/java/edu/illinois/cs465/boardrate/MainActivity.java
+++ app/src/main/java/edu/illinois/cs465/boardrate/MainActivity.java
@@ -1,6 +1,7 @@
 package edu.illinois.cs465.boardrate;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -9,6 +10,14 @@ import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.List;
 
 import edu.illinois.cs465.boardrate.databinding.ActivityMainBinding;
 
@@ -32,6 +41,45 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
+
+        readGamePageData(); // Read data from raw/mock_db_games_game_page.csv
+    }
+
+    private List<Game> allGames= new ArrayList<Game>();
+    private void readGamePageData(){
+        InputStream is = getResources().openRawResource(R.raw.mock_db_games_game_page);
+        BufferedReader reader = new BufferedReader(
+                new InputStreamReader(is, Charset.forName("UTF-8"))
+        );
+        String line = "";
+        try {
+            reader.readLine(); // skip the first row
+            while ((line = reader.readLine()) != null) {
+                //split the line by ','
+                String[] tokens = line.split(",");
+                //read data
+                Game game = new Game();
+                game.setGameID(Integer.parseInt(tokens[0]));
+                game.setName(tokens[1]);
+                game.setRating(tokens[2]);
+                game.setNumberofReviewrs(Integer.parseInt(tokens[3]));
+                game.setImageURL(tokens[4]);
+                game.setTimetoPlay(tokens[5]);
+                game.setRanking1(Integer.parseInt(tokens[6]));
+                game.setRankings2(Integer.parseInt(tokens[7]));
+                game.setNumberofPlayers(tokens[8]);
+                game.setTag1(tokens[9]);
+                game.setTag2(tokens[10]);
+                game.setTag3(tokens[11]);
+                game.setTag1Ranking(Integer.parseInt(tokens[12]));
+                game.setTag2Ranking(Integer.parseInt(tokens[13]));
+                game.setTag3Ranking(Integer.parseInt(tokens[14]));
+                allGames.add(game);
+                Log.d("Main Activity", "Just created " + game);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
