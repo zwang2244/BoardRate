c5c05aed424b1740b9b6478b31663d80ae69b2fc
Meha Kumar <mehaonweb@gmail.com>
2021-11-11 23:30:18 -0600

    Login and search fragments added, both icons added to bottom navbar, search working but login throwing error upon navigation bar click

---
+++ app/src/main/java/edu/illinois/cs465/boardrate/ListViewAdapter.java
@@ -0,0 +1,81 @@
+package edu.illinois.cs465.boardrate;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+public class ListViewAdapter extends BaseAdapter {
+
+    // Declare Variables
+
+    Context mContext;
+    LayoutInflater inflater;
+    private List<String> gamesList = null;
+    private ArrayList<String> arraylist;
+
+    public ListViewAdapter(Context context, List<String> gameList) {
+        mContext = context;
+        this.gamesList = gameList;
+        inflater = LayoutInflater.from(mContext);
+        this.arraylist = new ArrayList<String>();
+        this.arraylist.addAll(gamesList);
+    }
+
+    public class ViewHolder {
+        TextView name;
+    }
+
+    @Override
+    public int getCount() {
+        return gamesList.size();
+    }
+
+    @Override
+    public String getItem(int position) {
+        return gamesList.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    public View getView(final int position, View view, ViewGroup parent) {
+        final ViewHolder holder;
+        if (view == null) {
+            holder = new ViewHolder();
+            view = inflater.inflate(R.layout.fragment_card_view, null);
+            // Locate the TextViews in fragment_card_view.xml
+            holder.name = (TextView) view.findViewById(R.id.card_gametitle);
+            view.setTag(holder);
+        } else {
+            holder = (ViewHolder) view.getTag();
+        }
+        // Set the results into TextViews
+        holder.name.setText(gamesList.get(position));
+        return view;
+    }
+
+    // Filter Class
+    public void filter(String charText) {
+        charText = charText.toLowerCase(Locale.getDefault());
+        gamesList.clear();
+        if (charText.length() == 0) {
+            gamesList.addAll(arraylist);
+        } else {
+            for (String wp : arraylist) {
+                if (wp.toLowerCase(Locale.getDefault()).contains(charText)) {
+                    gamesList.add(wp);
+                }
+            }
+        }
+        notifyDataSetChanged();
+    }
+
+}
