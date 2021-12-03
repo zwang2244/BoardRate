93e17d792c84e41a37799e9b24a733d8bb0ec0d2
cpparnell <connorparnell2018@gmail.com>
2021-12-02 19:49:00 -0600

    WriteReview

--- app/src/main/java/edu/illinois/cs465/boardrate/WriteReviewActivity.java
+++ app/src/main/java/edu/illinois/cs465/boardrate/WriteReviewActivity.java
@@ -13,43 +13,70 @@ import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RatingBar;
+import android.widget.TextView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.textfield.TextInputEditText;
 
 import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class WriteReviewActivity extends AppCompatActivity {
 
+    private List<Game> allGames = new ArrayList<Game>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_write_review);
+        MyApplication myApplication = (MyApplication) getApplication();
+        allGames = myApplication.getAllGames();
 
 
+        RatingBar ratingBar = findViewById(R.id.ratingBar);
+        TextInputEditText text = findViewById(R.id.textInputBox);
         Button b = findViewById(R.id.button);
+
+        String gameTitle = getIntent().getStringExtra("gameTitle");
+        TextView whatDoYouThink = findViewById(R.id.whatDoYouThink);
+        whatDoYouThink.setText("What do you think about " + gameTitle + "?");
+
+        int gameId = 0;
+        for (Game g : allGames) {
+            if (g.getName().equals(gameTitle)){
+                gameId = g.getGameID();
+                break;
+            }
+        }
+        int finalGameId = gameId;
         b.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Snackbar snackbar = Snackbar.make(v, "Submission Confirmed!", Snackbar.LENGTH_LONG);
                 snackbar.show();
 
+                float numStars = ratingBar.getRating();
+                String comment = text.getText().toString().trim();
+                try {
+                    FileWriter writer = new FileWriter("mock_db_games_my_reviews.csv");
+                    writer.append("\n" + finalGameId + ", " + numStars + ", 0, " + comment);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
                 Handler handler = new Handler();
                 handler.postDelayed(new Runnable() {
                     public void run() {
-                        // Actions to do after 3 seconds
-                        startActivity(new Intent(WriteReviewActivity.this, GameDetailsActivity.class)); //reload page as blank
+                        // Actions to do after 1.5 seconds
+                        startActivity(new Intent(WriteReviewActivity.this, MainActivity.class)); //send back to main page
                     }
-                }, 3000);
+                }, 1500);
             }
         });
 
 
     }
 }
-
-/*
-TODO:
-- implement functionality that updates reviews database with user submitted reviews
- */
